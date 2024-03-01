baf2624a1beedcec6358b9bb13668622481af3ab
sourav <souravchongrey@gmail.com>
2024-03-01 23:28:38 +0530

    registration and login layout group routing layout

---
+++ app/auth/(with-auth-layout)/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+      <div>
+        <div>login and resgistration layout</div>
+          <div>{children}</div>
+          <div>place do it</div>
+      </div>
+  )
+}
+
+export default layout
