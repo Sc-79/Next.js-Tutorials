35276d4190f8118fe552fcefcae686fc5443c67b
sourav <souravchongrey@gmail.com>
2024-03-02 07:35:57 +0530

    file colocation

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+
+
+function Barchart(){
+    return(
+        <div>hi i am barchart</div>
+    )
+}
+export default function DashBoard(){
+    return(
+        <div>hi i am barchart</div>
+    )
+}
